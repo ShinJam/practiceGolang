41241a66a2dc8f6cb9ad4d868e6e66914d194aaa
ShinJam <nevvjann@gmail.com>
2021-11-25 23:42:42 +0900

    1-4 Functions part one

--- nomad/main.go
+++ nomad/main.go
@@ -11,6 +11,18 @@ func printDivider(word string) {
 	fmt.Printf("%v %v %v\n", repats, word, repats)
 }
 
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
 	printDivider("#1.1 Packages and Imports")
 
@@ -23,4 +35,9 @@ func main() {
 
 	fmt.Println(name1, name2, name3)
 
+	printDivider("#1.3 functions part one")
+	fmt.Println(multiply(13, 17))
+	_, upperName := lenAndUpper("jam") // ignore은 underscore(_)를 사용한다
+	fmt.Println(upperName)
+	repeatMe("A", "B", "C")
 }
