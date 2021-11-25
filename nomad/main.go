cedbeafc3d291a6d926c08615192ed61f12874cb
ShinJam <nevvjann@gmail.com>
2021-11-25 19:10:17 +0900

    - asign variables

--- nomad/main.go
+++ nomad/main.go
@@ -10,4 +10,11 @@ func main() {
 
 	something.Hello()
 	// something.bye() // error: ./main.go:12:2: cannot refer to unexported name something.bye
+
+	name1 := "jam"
+	var name2 = "jam"
+	var name3 string = "jam"
+
+	fmt.Println(name1, name2, name3)
+
 }
